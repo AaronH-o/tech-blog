6d92e12e799981a6f5d9ee2fbcdbd5cf864c5a7d
Aaron Ho <bicecreamaar@gmail.com>
2024-05-22 14:57:02 -0700

    added seed data

---
+++ seeds/seed.js
@@ -0,0 +1,30 @@
+const sequelize = require('../config/connection');
+const { User, BlogPost, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const blogPostData = require('./blogPostData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const blogPosts = await BlogPost.bulkCreate(blogPostData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const comments = await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+
+  process.exit(0);
+};
+
+seedDatabase();
