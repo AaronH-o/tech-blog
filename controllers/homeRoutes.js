29d42ddbb59a9611804e4ef5fc85422e1889b72d
Aaron Ho <bicecreamaar@gmail.com>
2024-05-22 15:31:02 -0700

    heroku deployment

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,17 +1,42 @@
 const router = require('express').Router();
-const {  } = require('../models');
+const { BlogPost, Comment, User } = require('../models');
 // Import the custom middleware
 // const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
+    const blogPostData = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain:true }));
+
+
     res.render('homepage', {
+      blogPosts,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
+});
+
+
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
