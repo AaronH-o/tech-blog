67b2edb007404f84f75119bcf8bc529288b2a22f
Aaron Ho <bicecreamaar@gmail.com>
2024-05-29 17:55:02 -0700

    fixed routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { BlogPost, Comment, User } = require('../models');
 // Import the custom middleware
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
